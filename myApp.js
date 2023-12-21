7c7a7290b4b9188fd2e405153e8f8692a83c32c8
thegunguy33 <78658443+thegunguy33@users.noreply.github.com>
2023-12-21 17:59:21 -0500

    Added Middleware

--- myApp.js
+++ myApp.js
@@ -1,10 +1,33 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+let bGround = require('fcc-express-bground');
+require("dotenv").config();
 
-console.log("Hello World");
-
-app.get("/", (req, res) =>  {
-    res.send('Hello Express')
+app.use(function (req, res, next) {
+  console.log(req.method + "" + req.path + "-" + req.ip);
+  next();
 });
 
- module.exports = app;
+//bGround.log("Hellow World");
+// console.log("Hello World");
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+//app.get("/json", (req, res) => {
+//  if (process.env["MESSAGE_STYLE"] == "uppercase") {
+//    res.json({
+//      message: "Hello Json".toUpperCase(),
+//    });
+//  } else {
+//    res.json({
+//      message: "Hello json",
+//    });
+//  }
+//});
+
+module.exports = app;
